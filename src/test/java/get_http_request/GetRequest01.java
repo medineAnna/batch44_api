11f50b1f361795741b3fd4ebab4f8d8dbbe760e0
medineAnna <medine.ah>
2022-03-01 22:07:58 +0100

    api

--- src/test/java/get_http_request/GetRequest01.java
+++ src/test/java/get_http_request/GetRequest01.java
@@ -1,10 +1,34 @@
 package get_http_request;
 
+import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class GetRequest01 {
     @Test
     public void test01(){
+        String url="https://restful-booker.herokuapp.com/booking";
 
+        Response response=given().when().get(url);
+
+        // response.prettyPrint();
+
+        System.out.println("Status Code: " + response.statusCode());
+
+        System.out.println("Content Type: " + response.contentType());
+
+        System.out.println("Test Time: " + response.time());
+
+        Assert.assertEquals(200,response.statusCode());
+
+        Assert.assertEquals("application/json; charset=utf-8",response.contentType());
+
+        response
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .contentType("application/json; charset=utf-8");
     }
 }
