c18afeba5595b0ec033e443bd0afb88447330739
medineAnna <medine.ah>
2022-03-11 23:14:36 +0100

    ders

---
+++ src/test/java/seleniumGrid/MarketSession01.java
@@ -0,0 +1,135 @@
+package seleniumGrid;
+
+public class MarketSession01 {
+    /*QA Tester => Verilen gorevleri test guvenligine uygun bir sekilde test edip neticesine gore sonucu raporlar
+        QA Analyst => Isin basindan sonuna kadar tum asamalarda kullanici gozunden uygul;amayi degerlendirir ve sonra test edip raporlar
+        QA Engineer => Bu kisi kalite kontrol yapar ve teknik gecmisi ile automation a doker
+        SDET => Greybox testing => framework yapariz, raporlama, utility olusturma, design
+        6 yillik tecrube
+        whitebox(beyaz kutu testi) vs blackbox testing
+        planning
+        defining
+        designing
+        development
+        testing
+        deployment /Maintenance
+        BRD / FRD
+        User story
+        User story
+        PO => Business temsilcisi / veya isin sahibi / Stakeholder
+        BA => Aslinda tum teknoloji ve business sorunlarina cozumler getirir ve dokumantasyon olusturur
+        User Story
+        id    tanimlama(description)  Acceptance Criteria
+        Test Case
+US_ID TC_ID  Test Objective precondition(onkosul) Test Steps(adimlar)
+Test data expected Result(beklenilen sonuc) actual result (karsilasilan sonuc)  Status
+253-36-3746  boyle bir SSN ile hata mesaji almamaliyim
+karsilasilan: hata mesaji almiyorum
+85478432758347 => Bununla hata mesaji almaliyim
+ actual result: hata mesaji aliyorum
+ Post request => 1000 100000
+Bug bulursak ne yapariz?
+Reproduce (Yeniden olustururuz)
+Tester arkadaslara danisirim
+Sonra Developer arkadasa danisirim
+Takima report eder ve Defect log (bug olusturmak)
+String name = "";
+135 000
+    Kendinizi tanitin
+    Ben UI ve Backend de development yapan bir Siftware Engineer / Full Stack Automation Engineer olarak calisiyorum
+    Ben UI Development kisminda Java Script, HTML, CSS, BootStrap, React kullandim ve Backend icin Java, SpringBoot, MongoDB, Hibernate, Restful Api gibi toollar kullandim.
+    Ben Control version amacli Git ve GitHub i kullandim.
+    Project management tool olarak Jira kullandik
+    Agirlikli olarak Agile ortaminda bulundum, ama Waterfall u da cok iyi bilirim.
+    Acikcasi sizde yeni toollar gordum ve benim gelismim acisindan cok iyi bir etkisi olacagini dusunuyorum
+    Projenizden bahsedin
+            Header
+            Adiniz
+            unvaniniz(SDET)      adres
+                               email
+                               numara
+        Objective
+    Skills:
+    languages: Java, HTML, SQL, JavaScript, Python
+    frameworks: JUNIT, Cucumber BDD, TestNG, Springboot, spring, springMVC
+    tools: Cypress, Selenide, Appium
+    professional experience:
+    client: HMC   ,New York City, NY         ay/yil - present
+    title:SDET
+    (QA)BDD Cucumber projesini olusturduk ve POM kullandik. Beraberinde Java, Selenium WebDriver ve API kullandik.
+    (Dev)SpringBoot framework kullandik, Java, Hibernate, Rstful api kullanarak design ettik
+    FrontEnd Developer => Sadece UI development yapar
+    Backend Developer => Sadece Backend Developer olarak calisirsiniz
+    Full Stack Java Developer => Hem UI hemde Backend Developer olarak ise girebilirsiniz
+    proje: UI testing, Api, Database
+    Full Stack Automation Engineer => hem UI hem de Backend de automation yapabilecek kimseler
+    UI => UI dogrulama(validation)
+    Api => Api dogrulama yapabilirim
+    Database => Database dogrulama
+    FrontEnd => Selenium
+    Backend => Api + Database
+    Happy Path
+    SSN
+    isim
+    soyisim
+    adres
+    tel
+    kullanici adi
+    email sifre
+    Happy Path Scenario
+    Ben bunu ayni zamanda API ile dogrulamasini yaparsam
+    End To End
+    END TO END => Selenium + Api || Selenium + Database || Api + Database
+    SDET
+    Kac test case yazdiniz?
+    700 - 800
+    700 test case
+    Recruiter => Taseron firmadir
+    FreddieMac =>
+    Apex:
+    FreddieMac:
+    FreddiMac
+    Apex
+    Apple
+    Rainpark Solutions
+    Deeps Bank
+    Medunna Health
+    Hotel Management Systems
+    QA
+    Developerlar uygulama uzerinde calisirken siz ne yaparsiniz?
+    Requirement Analysis(gereksinimleri analize etmek)
+    Data olustururum
+    Test case development(Test case olustururuz)
+    Developer
+    Eger size requirements(gereksinimler )verilmezse ne yaparsiniz?
+    BA veya PO a sorarim
+    Daha once yapilmis benzeri uygalamalara bakarim
+    Test case                  Test Steps
+                                medunna.com
+                                Registration git
+                                SSN i gir
+                                SSN mesajini dogrula
+    API =>
+    link => endpoint
+    base url(ana url)
+    https://medunna.com/api/registrantions?name=""
+    Otel Odasi:
+    hmc.com/rooms?name="Ali" && lastname="Can"
+    room1
+    name: Ali
+    Soyisim: Can
+    id: 01
+    room2
+    Ahmet
+    Can
+    02
+    room3
+    Ayse
+    Tan
+    03
+    room4
+rooms
+      Can
+
+     */
+}
